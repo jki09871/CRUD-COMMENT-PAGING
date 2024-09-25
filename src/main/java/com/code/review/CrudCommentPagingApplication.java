e0119c5644a77375db1f93546f09346c62a88efc
jki09871 <ghdwjdrl56@naver.com>
2024-09-26 01:58:57 +0900

    first commit

---
+++ src/main/java/com/code/review/CrudCommentPagingApplication.java
@@ -0,0 +1,13 @@
+package com.code.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCommentPagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCommentPagingApplication.class, args);
+    }
+
+}
